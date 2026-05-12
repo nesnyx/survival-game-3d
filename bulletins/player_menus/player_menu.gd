6c2deaf2c39b089502bb3ccb76e6a764eeb6bedf
Aris Setiawan Bara <baraaris2@gmail.com>
2026-05-12 18:53:44 +0800

    feat: implement player inventory and crafting menu systems with associated item resources

--- bulletins/player_menus/player_menu.gd
+++ bulletins/player_menus/player_menu.gd
@@ -1,5 +1,6 @@
 extends Bulletin
 @onready var inventory_container: GridContainer = %InventoryContainer
+@onready var item_description_label: Label = %ItemDescriptionLabel
 
 func _enter_tree() -> void:
 	EventSystem.INV_inventory_updated.connect(update_inventory_slots)
@@ -9,6 +10,10 @@ func _ready() -> void:
 	EventSystem.PLA_freeze_player.emit()
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	EventSystem.INV_ask_update_inventory.emit()
+	for inventory_slot in inventory_container.get_children():
+		inventory_slot.mouse_entered.connect(show_item_info.bind(inventory_slot))
+		inventory_slot.mouse_exited.connect(hide_item_info)
+		
 	
 func update_inventory_slots(inventory : Array) -> void:
 	for i in inventory.size():
@@ -19,3 +24,19 @@ func close() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	EventSystem.BUL_destroy_bulletin.emit(BulletinConfig.Keys.CraftingMenu)
 	EventSystem.PLA_unfreeze_player.emit()
+	
+	
+func show_item_info(inventory_slot : InventorySlot)->void:
+	var item_key = inventory_slot.item_key
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) or item_key == null:
+		return
+	var item_resource := ItemConfig.get_item_resource(item_key)
+	
+	item_description_label.text = ItemConfig.get_item_resource(item_key).display_name + "\n" + item_resource.description
+	
+		
+func hide_item_info()->void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		return
+	item_description_label.text =""
+	
--- game/configs/item_config.gd
+++ game/configs/item_config.gd
@@ -2,10 +2,39 @@ class_name ItemConfig
 
 
 enum Keys {
+	Log,
+	Coal,
+	Mushroom,
+	Flintstone,
+	Fruit,
+	RawMeat,
+	CookedMeat,
 	Stick,
 	Stone,
 	Plant,
+	
+	Axe,
+	Pickaxe,
+	Campfire,
+	Multitool,
+	Rope,
+	Tinderbox,
+	Torch,
+	Tent,
+	Raft
 }
+
+const CRAFTABLE_ITEM_KEYS:Array[Keys] = [
+	Keys.Axe,
+	#Keys.Pickaxe,
+	#Keys.Campfire,
+	#Keys.Multitool,
+	Keys.Rope,
+	#Keys.Tinderbox,
+	#Keys.Torch,
+	#Keys.Tent,
+	#Keys.Raft
+]
 
 const ITEM_RESOURCE_PATHS := {
 	Keys.Stick : "res://resources/item_resources/stick_resource.tres",
